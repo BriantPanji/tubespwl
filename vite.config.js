dc04eec034d991124e71b9c8bfe3ea5df940e6ba
BriantPanji <panjibriant@students.usu.ac.id>
2025-04-13 11:28:00 +0700

    Konfigurasi Host & Port untuk Vite & Menambah package 'dotenv'

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: process.env.VITE_DEV_HOST,
+            port: 5173,
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
