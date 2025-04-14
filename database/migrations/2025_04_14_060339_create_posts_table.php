e089def995f7322e776558dc4f5332aa9da3d006
BriantPanji <panjibriant@students.usu.ac.id>
2025-04-14 14:40:47 +0700

    Membuat migration

---
+++ database/migrations/2025_04_14_060339_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('title', 128);
+            $table->string('content', 5120);
+            $table->string('location', 512);
+            $table->string('gmap_url', 512);
+            $table->string('place_name', 512);
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2025_04_14_064525_create_tag_pivots_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tag_pivots', function (Blueprint $table) {
+            $table->foreignId('hashtag_id')->references('id')->on('hashtags')->onDelete('cascade');
+            $table->foreignUlid('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tag_pivots');
+    }
+};
