6315ebc1bb8a80b9b83d05177798fa79a5e88b10
Ocirled <delricolie111@gmail.com>
2025-04-24 15:54:34 +0700

    Responsive beranda, detail post, komentar statis

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
-use App\Http\Controllers\RegisterUserController;
-use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisterUserController;
 
+//Beranda
+Route::get('/', [PostController::class, 'posts_view']);
 
-//TES BERANDA
-Route::get('/', function () {
-    return view('tes');
-});
+Route::get('/post/{post}', [PostController::class, 'post_detail']);
 
-
+// Auth
 Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::get('/register', [RegisterUserController::class, 'create'])->name('register');
 Route::post('/register', [RegisterUserController::class, 'store']);
-
 Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth')->name('logout');
+
+// Post
 
 Route::get('/tes', function () {
     return view('welcome');
