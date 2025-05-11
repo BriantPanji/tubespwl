61736b84061a32f5c8fec2086d5518291d82e873
Sutanto-cell <sutantolie15@gmail.com>
2025-05-11 10:50:16 +0700

    buat halaman dashboard admin reported comment

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function showPost()
     {
         $reportedPosts = Post::whereHas('reports') // Hanya ambil postingan yang punya laporan
             ->with(['reports', 'user']) // Memuat relasi 'reports' (pelapor) dan 'user' (pemilik post)
             ->get();
 
-        return view('post.reported', compact('reportedPosts'));
+        return view('admin.reported', compact('reportedPosts'));
+    }
+
+    public function showCom(){
+        $reportedComs = Comment::whereHas('reports')
+        ->with(['reports', 'user'])
+        ->get();
+
+        return view('admin.comment', compact('reportedComs'));
     }
 }
