e61490875a069696f54e1ec2f1cf2ce4898bf523
BriantPanji <panjibriant@students.usu.ac.id>
2025-05-25 00:31:38 +0700

    tesprodd

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import dotenv from 'dotenv';
-dotenv.config();
 
-let PORT = 5173
 export default defineConfig({
-    server: {
-        host: true,
-        hmr: {
-            host: process.env.VITE_DEV_HOST,
-            port: PORT,
-        },
-        cors: true,
-        origin: 'http://'+ process.env.VITE_DEV_HOST+':'+ PORT
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
