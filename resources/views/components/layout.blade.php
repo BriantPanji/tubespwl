b5e3eafb3e74924c7563e9a219815a80aa7e5073
Sutanto-cell <sutantolie15@gmail.com>
2025-06-12 16:28:03 +0700

    aside kiri

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -24,7 +24,8 @@
     <title>{{ $title }}</title>
 </head>
 
-<body x-init="console.log('ALPINEJS INITIALIZE')" class="min-w-full max-w-full min-h-screen w-full h-full bg-sl-base text-sl-text">
+<body x-init="console.log('ALPINEJS INITIALIZE')"
+    class="min-w-full max-w-full min-h-screen w-full h-full bg-sl-base text-sl-text relative">
     @if ($errors->has('banned'))
         <script>
             Swal.fire({
@@ -35,10 +36,62 @@
         </script>
     @endif
     <x-header></x-header>
-    <main
-        class="px-5 pt-1 pb-7 min-w-full max-w-full min-h-screen w-full h-auto bg-sl-base text-sl-text xs:px-12 sm:px-22 md:px-34 lg:px-64 xl:px-90 2xl:px-110 flex flex-col gap-y-3">
-        {{ $slot }}
-    </main>
+    <div class="flex w-full min-h-screen justify-center">
+        <aside class="w-1/5 h-screen bg-sl-base fixed border-r-hairline border-r-gray-500/20 p-4 left-0 z-1">
+            <a href="/" class="flex items-center gap-3 p-2 rounded hover:bg-gray-500/50 mb-4">
+                <i class="fa-light fa-house text-lg w-5 h-5"></i>
+                <span class="font-medium">Beranda</span>
+            </a>
+
+            <a href="/profile" class="flex items-center gap-3 p-2 rounded hover:bg-gray-500/20 mb-4">
+                <i class="fa-light fa-user text-lg w-5 h-5"></i>
+                <span class="font-medium">Profil</span>
+            </a>
+
+            <a href="/my/post" class="flex items-center gap-3 p-2 rounded hover:bg-gray-500/20 mb-4">
+                <i class="fa-light fa-rectangle-history text-lg w-5 h-5"></i>
+                <span class="font-medium">Postingan Saya</span>
+            </a>
+
+            <a href="/my/comments" class="flex items-center gap-3 p-2 rounded hover:bg-gray-500/20 mb-4">
+                <i class="fa-light fa-comments text-lg w-5 h-5"></i>
+                <span class="font-medium">Komentar Saya</span>
+            </a>
+
+            <a href="/my/votes" class="relative flex items-center gap-3 p-2 rounded hover:bg-gray-500/20 mb-4">
+                <i class="fa-light fa-up text-lg w-5 h-5"></i>
+                <span class="font-medium">Votingan Saya</span>
+            </a>
+
+            <a href="/my/bookmarks" class="flex items-center gap-3 p-2 rounded hover:bg-gray-500/20 mb-4">
+                <i class="fa-light fa-bookmark text-lg w-5 h-5"></i>
+                <span class="font-medium">Tersimpan</span>
+            </a>
+
+            <form action="{{ route('logout') }}" method="POST">
+            @csrf
+                <button
+                    type="submit"
+                    class="flex items-center gap-3 p-2 rounded hover:bg-red-800 mb-4 w-full">
+                    <i class="fa-light fa-arrow-right-from-bracket text-lg w-5 h-5"></i>
+                    <span class="font-medium">Logout</span>
+                </button>
+            </form>
+        </aside>
+
+        <main class="w-1/2 px-6 py-4 bg-sl-base text-sl-text flex flex-col gap-y-3">
+            {{ $slot }}
+        </main>
+
+        <aside class="w-1/5 bg-sl-tertiary p-4 fixed right-0">
+            <h2 class="text-lg font-bold mb-4">Info Tambahan</h2>
+            <ul class="space-y-2">
+                <li><a href="#" class="hover:underline">asd</a></li>
+                <li><a href="#" class="hover:underline">asd</a></li>
+                <li><a href="#" class="hover:underline">asd</a></li>
+            </ul>
+        </aside>
+    </div>
     <x-footer></x-footer>
     <script src="{{ asset('js/swaldef.js') }}" defer></script>
 </body>
