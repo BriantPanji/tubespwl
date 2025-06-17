68da1c80d7e63cfabe3d5eacb2beb12144c32135
BriantPanji <panjibriant@students.usu.ac.id>
2025-06-17 21:33:19 +0700

    Refactor Vite configuration to conditionally set server settings based on environment

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,40 @@ import tailwindcss from '@tailwindcss/vite';
 import dotenv from 'dotenv';
 dotenv.config();
 
-let PORT = 5173
-export default defineConfig({
-    server: {
-        host: true,
-        hmr: {
-            host: process.env.VITE_DEV_HOST,
-            port: PORT,
+let confignya;
+if (process.env.APP_ENV !== 'local') {
+    console.warn('Vite is running in non-local environment. Ensure that your configuration is correct.');
+    confignya = {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+    };
+
+} else {
+    console.log('Vite is running in local environment.');
+    let PORT = 5173
+    confignya = {
+        server: {
+            host: true,
+            hmr: {
+                host: process.env.VITE_DEV_HOST,
+                port: PORT,
+            },
+            cors: true,
+            origin: 'http://'+ process.env.VITE_DEV_HOST+':'+ PORT
         },
-        cors: true,
-        origin: 'http://'+ process.env.VITE_DEV_HOST+':'+ PORT
-    },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+    };
+}
+
+export default defineConfig(confignya);
